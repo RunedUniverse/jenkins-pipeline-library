2e5eff1fd95f508117001429f4c4060224941695
VenaNocta <venanocta@gmail.com>
2024-02-17 19:52:03 +0100

    added extended info

--- src/net/runeduniverse/lib/tools/jenkins/MavenProject.groovy
+++ src/net/runeduniverse/lib/tools/jenkins/MavenProject.groovy
@@ -4,6 +4,7 @@ class MavenProject implements Project {
 
 	def workflow;
 
+	private String name = "";
 	private String path = ".";
 	private String modulePath = ".";
 	private MavenProject parent = null;
@@ -13,6 +14,10 @@ class MavenProject implements Project {
 		this.workflow = workflow;
 	}
 
+	public MavenProject setName(name) {
+		this.name = name;
+		return this;
+	}
 	public MavenProject setPath(path) {
 		this.path = path;
 		return this;
@@ -32,6 +37,15 @@ class MavenProject implements Project {
 		return this;
 	}
 
+	public MavenProject addModule(Map conf) {
+		MavenProject project = new MavenProject(this.workflow)
+				.setName(conf.name)
+				.setPath(conf.path)
+				.setModulePath(conf.modulePath);
+		this.addModule(project);
+		return project;
+	}
+
 	public String getVersion(modulePath = null) {
 		String modPath = modulePath == null ? "." : modulePath;
 
@@ -46,8 +60,23 @@ class MavenProject implements Project {
 					});
 			return version;
 		}
-
-		modPath = modulePath == null ? this.modulePath : this.modulePath + '/' + modulePath;
+		modPath = this.modulePath == null ? this.path : this.modulePath;
+		if(modulePath != null) {
+			modPath = modPath + '/' + modulePath;
+		}
 		return this.parent.getVersion(modPath);
+	}
+
+	public void info() {
+		this.workflow.sh("echo name:       " + this.name);
+		this.workflow.sh("echo path:       " + this.path);
+		if(this.modulePath != null)
+			this.workflow.sh("echo modulePath: " + this.modulePath);
+		this.workflow.sh("echo version:    " + this.path);
+
+		this.modules.each {
+			this.workflow.sh("-------------------------");
+			it.info();
+		}
 	}
 }
