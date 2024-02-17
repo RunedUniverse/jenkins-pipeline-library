5467e2964d670c1d292b85498557fb0ea584a900
VenaNocta <venanocta@gmail.com>
2024-02-17 16:40:12 +0100

    testing groovy print 7.1

--- src/net/runeduniverse/lib/tools/jenkins/PipelineBuilder.groovy
+++ src/net/runeduniverse/lib/tools/jenkins/PipelineBuilder.groovy
@@ -10,8 +10,8 @@ class PipelineBuilder implements Serializable {
 	}
 
 	def mvn(args) {
-		this.workflow.sh "Method 1: ${this.workflow.GLOBAL_MAVEN_SETTINGS}"
-		this.workflow.sh "Method 2: ${this.workflow.getProperty("GLOBAL_MAVEN_SETTINGS")}"
+		this.workflow.sh "echo Method 1: ${this.workflow.GLOBAL_MAVEN_SETTINGS}"
+		this.workflow.sh "echo Method 2: ${this.workflow.getProperty("GLOBAL_MAVEN_SETTINGS")}"
 		//steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
 	}
 	
